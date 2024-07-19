ff9fefdcf9838bf5ef61a419e54f66c1a105675e
CVM <s778193047@gmail.com>
2024-07-19 22:00:58 +0530

    nucleus final project

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Carousel} from "react-responsive-carousel";
-// import img1 from "../assets/3.jpg";
-// import img2 from "../assets/4.jpg";
+
 import i1 from "../assets/10.png"
 import i2 from "../assets/20.png"
 import i3 from "../assets/30.png"
